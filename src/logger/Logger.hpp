087d490c86c1884da24bb98f856c7f880c13c343
Sima214 <anastasios2014@gmail.com>
2018-03-15 00:13:11 +0200

    More cpp logger. Of cource I forgot something.

--- src/logger/Logger.hpp
+++ src/logger/Logger.hpp
@@ -7,6 +7,23 @@ C_DECLS_START
 C_DECLS_END
 
 namespace ssce {
-
+template <class... T> void logv(T... args) {
+  ssce_logv(args...);
+}
+template <class... T> void logd(T... args) {
+  ssce_logd(args...);
+}
+template <class... T> void logi(T... args) {
+  ssce_logi(args...);
+}
+template <class... T> void logw(T... args) {
+  ssce_logw(args...);
+}
+template <class... T> void loge(T... args) {
+  ssce_loge(args...);
+}
+template <class... T> void logf(T... args) {
+  ssce_logf(args...);
+}
 }
 #endif /*SSCE_LOGGER_HPP*/
