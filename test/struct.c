07abb2b9a1212134034ee1c113c6669653ae21d0
Sima214 <anastasios2014@gmail.com>
2018-06-19 01:04:43 +0300

    Now the test is actually a test.

--- test/struct.c
+++ test/struct.c
@@ -3,28 +3,83 @@
 #include <structures/Interface.h>
 #include <structures/Sort.h>
 #include <stddef.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
 
-static int char_cmp_eq(const char* data, size_t i, size_t j) {
+#define KBYTES(kb) kb*1024
+#define MBYTES(mb) KBYTES(mb*1024)
+
+static int int_cmp_eq(const int* data, size_t i, size_t j) {
   return data[i] == data[j];
 }
 
-static int char_cmp_l(const char* data, size_t i, size_t j) {
+static int int_cmp_l(const int* data, size_t i, size_t j) {
   return data[i] < data[j];
 }
 
-static int char_cmp_le(const char* data, size_t i, size_t j) {
+static int int_cmp_le(const int* data, size_t i, size_t j) {
   return data[i] <= data[j];
 }
 
-static void char_swap(char* data, size_t i, size_t j) {
-  char tmp = data[i];
+static void int_swap(int* data, size_t i, size_t j) {
+  int tmp = data[i];
   data[i] = data[j];
   data[j] = tmp;
 }
 
+const DataTypeInterface iinterface = {sizeof(char), (Compare) int_cmp_eq,
+  (Compare) int_cmp_l, (Compare) int_cmp_le, (Operate) int_swap};
+
+/*
+ * Fills an array with random data.
+ */
+static void fill_garbage(void* array, size_t bytes) {
+  size_t words = bytes/4;
+  bytes = bytes%4;
+  int* p;
+  int* const end_words = ((int*)array) + words;
+  char* const end_bytes = ((char*)end_words) + bytes;
+  for(p=array; p<end_words; p++) {
+    int r = rand();
+    *p = r;
+  }
+  char* q;
+  for(q=p; q<end_bytes; q++) {
+    char r = rand()%256;
+    *q = r;
+  }
+}
+
+static int is_sorted_i(int* a, size_t n) {
+  size_t i = 0;
+  for(size_t i=1; i<n; i++) {
+    if(!(a[i] >= a[i-1])) {
+      return 0;
+    }
+  }
+  return 1;
+}
+
 int main(int argc, char* argv[]) {
-  char test[] = {0, 31, 4, 11, 24, 12, 1, 8, 2, 1, 2, 4, 5, 1, 21, 32, 13, 12, 21, 51, 53, 52, 41, 221, 3, 34, 38, 23, 45, 63, 0, 0};
-  const DataTypeInterface i = {sizeof(char), (Compare) char_cmp_eq, (Compare) char_cmp_l, (Compare) char_cmp_le, (Operate) char_swap};
-  const DataTypeInterface i0 = {sizeof(char), NULL, NULL, NULL, NULL};
-  heapsort(test, sizeof(test)/sizeof(char), &i0);
+  srand(time(NULL));
+  // Source data structure.
+  static int garbage[MBYTES(4)];
+  static int test_area[MBYTES(4)];
+  fill_garbage(garbage, sizeof(garbage));
+  memcpy(test_area, garbage, sizeof(test_area));
+  /*
+   * Split arrays to 64 chunks.
+   */
+  for(size_t i=0; i<sizeof(test_area)/sizeof(int)/64; i++) {
+    int* array = test_area + (i*64);
+    heapsort(array, 64, &iinterface);
+  }
+  for(size_t i=0; i<sizeof(test_area)/sizeof(int)/64; i++) {
+    int* array = test_area + (i*64);
+    if(!is_sorted_i(array, 64)) {
+      return EXIT_FAILURE;
+    }
+  }
+  return EXIT_SUCCESS;
 }
