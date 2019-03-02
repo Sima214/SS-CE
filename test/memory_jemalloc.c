1b2dae411aaf47d9adcab8abc0a866a20032c6bd
Anastasios Simeonidis <anastasios2014@gmail.com>
2019-03-02 02:13:52 +0200

    Add jemalloc test.

--- test/memory_jemalloc.c
+++ test/memory_jemalloc.c
@@ -1,7 +1,21 @@
 #include "test_utils.h"
 
+#include <jemalloc.h>
 #include <Macros.h>
 
+#include <stdlib.h>
+
+void leak(size_t c) {
+  // Leak some memory.
+  volatile int* leakage = malloc(c * 32);
+  for(size_t i = 0; i < c; i++) {
+    leakage[i * 16] = leakage[i * 16 + 1];
+  }
+}
+
 int main(MARK_UNUSED int argc, MARK_UNUSED char* argv[]) {
-  return EXIT_FAILURE;
+  malloc_stats_print(NULL, NULL, NULL);
+  leak(1024);
+  malloc_stats_print(NULL, NULL, NULL);
+  return EXIT_SUCCESS;
 }
